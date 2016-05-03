930b0fd42ae88dc7d5a6260505defff2294b54fa
Peter Jang <peter.x.jang@gmail.com>
2016-05-03 14:28:11 -0500

    config webpack for babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,17 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
   }
 };
